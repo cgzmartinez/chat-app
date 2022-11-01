f9274b37e80c4974d1c05ec2994e2de572e1dade
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2022-11-01 14:34:47 -0400

    Added GiftedChat and Bubble to Chat.js
    Added custom messages to initial switch to the chat screen, as well as dates when messages are sent.

--- components/Chat.js
+++ components/Chat.js
@@ -1,27 +1,89 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, } from 'react-native';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat'
+import { StyleSheet, Platform, KeyboardAvoidingView } from 'react-native';
 
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
+  }
+
   componentDidMount() {
     // Sets the name to be included in the nav bar
     let name = this.props.route.params.name;
+
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: `Hello, ${name}!`,
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+        {
+          _id: 2,
+          text: `${name} entered the chat`,
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    });
+
     this.props.navigation.setOptions({ title: name });
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#32353b',
+          },
+          left: {
+            backgroundColor: '#ffffff',
+          },
+        }}
+      />
+    );
   }
 
   render() {
     // Sets color value as background color for the chat screen
     let color = this.props.route.params.color;
     return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: color,
-        }}
-      >
-        <Text>This is the Chat screen!</Text>
+      <View style={[styles.container, { backgroundColor: color }]}>
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {Platform.OS === 'android' ? (
+          <KeyboardAvoidingView behavior="height" />
+        ) : null}
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
